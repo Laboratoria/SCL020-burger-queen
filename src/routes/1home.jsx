3f8dbe89b37d9be5b6e1e06baeb6ba83cee83ae1
Iankat <ianka.ferrada@gmail.com>
2022-08-01 08:48:49 -0400

    logo y otras imagenes

--- src/routes/1home.jsx
+++ src/routes/1home.jsx
@@ -1,23 +1,30 @@
-import {Link} from "react-router-dom";
-const Home= () => {
-    return (
-      <>
-      <div class= "home">
+import { Link } from "react-router-dom";
+// const logo = require.context ('../../assets/img', true);
+
+const Home = () => {
+  
+
+  return (
+    <>
+      <div class="home">
         <main>
-         
+
           <div class="title">
-            <h1 className="text-3xl font-bold underline">Burger Queen </h1>
-            </div>
-            <div class="subtitle">
+          {/* <img src={logo (`./lemons.png'`)} /> */}
+            <h1 className="text-3xl font-bold underline">Lemon Restaurant </h1>
+          </div>
+          <div class="subtitle">
             <p>Food ordering App</p>
-            </div>
-            
+          </div>
+
         </main>
         <nav class="btnContinuar">
           <Link to="/login">Continuar</Link>
         </nav>
-        </div>
-      </>
-    );
-  }
-  export default Home
+      </div>
+    </>
+  );
+}
+export default Home
+// 
+// 
---
+++ src/routes/3components.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const mesas = () => {
+  return (<div className="btnGrupo">
+  <button className="mesas">1</button>
+  </div>
+    // <div>mesas</div>
+  )
+}
