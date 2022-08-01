377d7256ff3e8f935471a94b1580cf122256bd7c
genesis <gnsslr@gmail.com>
2022-08-01 16:49:56 -0400

    Contexto listo y activo

--- src/index.js
+++ src/index.js
@@ -3,25 +3,21 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
+import { OrderProvider } from './context/OrderContext';
 
 //import reportWebVitals from './reportWebVitals';
 //COMPONENTE RAÍZ
 //ReactDOM.render(<div>Hola Mundo</div>, document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <OrderProvider>
   <BrowserRouter>
   <App/>
   </BrowserRouter>
+  </OrderProvider>
   
-  /*<React.StrictMode>
-    <App />
-  </React.StrictMode>*/
 );
-  /*<React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
