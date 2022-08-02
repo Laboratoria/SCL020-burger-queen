d63e7cf3c2c08511db2cd4bb703fb2c342fa9fca
Iankat <ianka.ferrada@gmail.com>
2022-08-02 12:13:55 -0400

    refactorizando

--- src/routes/3menu.jsx
+++ src/routes/3menu.jsx
@@ -1,40 +1,30 @@
-import {Link} from "react-router-dom";
-import Button from "../Components/Common/button";
-import Card from "../card/card";
+import React from "react";
+import { Link } from "react-router-dom";
+import Table from "../Components/Common/tables.jsx";
+import Menus from "../Components/Common/menu.jsx";
+import Card from "../card/card.jsx";
 
-const Menu= () => {
-    return (
-      <>
-        <main>
-          <div class="mesas" >
-           <Button class="mesa" label={1}/>
-           <Button label={2}/>
-           <Button label={3}/>
-           <Button label={4}/>
-           <Button label={5}/>
-          </div>
-          
-          <div className="nameClient">
-          <input placeholder="Nombre del cliente"/>
-          </div >
-          <div className="menu">
-          <button className="btnMenu">Menú vegetariano</button>
-          
-          <button className="btnMenu">Menú de carnes</button>
-          <button className="btnMenu">Agregados</button>
-          </div>
-          <Card title={"Lomo Saltado"} image={".src/"} price={"2500"}/>
-          <Card title={"Lomo Saltado"} image={".src/"} price={"2500"}/>
-          <Card title={"Lomo Saltado"} image={".src/assets/logo.png"} price={"2500"}/>
-        </main>
-        <nav className="btnSendOrderF">
-          <div className="btnSendOrderCh">
-           <Link to="/cocina">Enviar pedido a cocina</Link>
-           </div>
-        </nav>
-      </>
-    );
-  }
-  export default Menu
-  //prueba
-  
+const Menu = () => {
+  return (
+    <>
+      <div className="nameClient">
+        {/* value name mesero */}
+        <Table />         {/* numero mesas */}
+      </div >
+      <div className="menu">
+        <Menus />
+      
+      </div>
+      <Card/>
+      {/* <Card title={"Lomo Saltado"} image={".src/"} price={"2500"} />
+      <Card title={"Lomo Saltado"} image={".src/"} price={"2500"} />
+      <Card title={"Lomo Saltado"} image={".src/assets/logo.png"} price={"2500"} /> */}
+      <nav className="btnSendOrderF">
+        <div className="btnSendOrderCh">
+          <Link to="/cocina">Enviar pedido a cocina</Link>
+        </div>
+      </nav>
+    </>
+  );
+}
+export default Menu
