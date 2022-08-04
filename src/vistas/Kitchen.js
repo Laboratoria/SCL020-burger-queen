147de437de5d2aa97d6e71bb11ad68d7027cc513
genesis <gnsslr@gmail.com>
2022-08-04 18:58:02 -0400

    Comandas en cocina

--- src/vistas/Kitchen.js
+++ src/vistas/Kitchen.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import TitlesHeader from "../components/TitlesHeader";
 import { db,  collection } from '../firebase';
+import {Command} from '../components/Command'
 import { onSnapshot,
     doc,
     query,
@@ -8,30 +9,44 @@ import { onSnapshot,
 
 
 function Kitchen() {
+
+    const [orders, setOrders] = useState([])
+
+    const callbackO = (data) => {
+        return setOrders(data.docs.map((order) => {
+            return ({ ...order.data() })
+          }))
+    }
+        // data.docs.map((order) => {
+        //     console.log(data.docs)
+        // })
+        // console.log(data)
+        // }
    
      useEffect (() => {
         const updatingPublications = () => {
             const q = query(collection(db, "waitersOrders"));
-            // console.log(q)
-            onSnapshot(q, callback);
+            onSnapshot(q, callbackO);
           };
           updatingPublications()
 
     }, [])
-    const callback = (data) => {
-        data.docs.map((order) => {
-            console.log(data.docs)
-        })
-        console.log(data)
-        }
-    
-
+    console.log(orders)
+    // console.log(orders[0].items[0].name)
+orders.map((order, i) => (
+    order.items.map((item) => (
+        console.log(item)
+    ))
+))
     return(
         <>
 <TitlesHeader title = 'Cocina'/>
-<div className = 'kitchenCardsArea'>
-  
-    
+<div className = 'kitchenCommandsArea'>
+
+  {orders.map((order, i) => (
+    <Command order = {order}/>
+   
+        ))}
 </div>
 </>
     )
